0b8279c9c2d02c3e4200a9dd1b1dd04585888788
Uri Nemirovsky <urinemirovsky@yahoo.com>
2023-05-04 11:13:20 -0400

    completed exercise

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-require('dotenv').config('.env');
-const cors = require('cors');
-const express = require('express');
+require("dotenv").config(".env");
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 const { PORT = 3000 } = process.env;
+const { auth } = require("express-openid-connect");
 // TODO - require express-openid-connect and destructure auth from it
 
-const { User, Cupcake } = require('./db');
+const { User, Cupcake } = require("./db");
 
 // middleware
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 /* *********** YOUR CODE HERE *********** */
 // follow the module instructions: destructure config environment variables from process.env
 // follow the docs:
-  // define the config object
-  // attach Auth0 OIDC auth router
-  // create a GET / route handler that sends back Logged in or Logged out
+// define the config object
+// attach Auth0 OIDC auth router
+// create a GET / route handler that sends back Logged in or Logged out
 
-app.get('/cupcakes', async (req, res, next) => {
+const { AUTH0_SECRET, AUTH0_BASE_URL, AUTH0_CLIENT_ID, AUTH0_AUDIENCE } =
+  process.env;
+
+const config = {
+  authRequired: true,
+  auth0Logout: true,
+  secret: AUTH0_SECRET,
+  baseURL: AUTH0_AUDIENCE,
+  clientID: AUTH0_CLIENT_ID,
+  issuerBaseURL: AUTH0_BASE_URL,
+};
+
+app.use(auth(config));
+
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+app.get("/cupcakes", async (req, res, next) => {
   try {
     const cupcakes = await Cupcake.findAll();
     res.send(cupcakes);
@@ -33,12 +52,11 @@ app.get('/cupcakes', async (req, res, next) => {
 
 // error handling middleware
 app.use((error, req, res, next) => {
-  console.error('SERVER ERROR: ', error);
-  if(res.statusCode < 400) res.status(500);
-  res.send({error: error.message, name: error.name, message: error.message});
+  console.error("SERVER ERROR: ", error);
+  if (res.statusCode < 400) res.status(500);
+  res.send({ error: error.message, name: error.name, message: error.message });
 });
 
 app.listen(PORT, () => {
   console.log(`Cupcakes are ready at http://localhost:${PORT}`);
 });
-
